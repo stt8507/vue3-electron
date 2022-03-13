a46cb76795a489bade78f284bc31fb81028d45bc
samsonk88507 <stt8507@gmail.com>
2022-03-13 18:13:19 +0800

    加入Router及Bootstrap5

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class = "container">
+    <img class = "img-fluid" alt = "Vue logo" src = "../assets/logo.png" />
+    <h1 class = "display-1 text-bold"> My App </h1>
+    <h1 class = "text-gray-soft text-regular"> Greetings to you all. </h1>
+    <button class="btn btn-primary" type="button">123</button>
+  </div>
+</template>
